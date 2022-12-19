01ac530f8d48319c7b70a034435f2bb691c9b859
saqib-devops <saqibahmad778866@gmail.com>
2022-12-19 12:21:16 -0500

    Website: Graphs added

--- src/website/urls.py
+++ src/website/urls.py
@@ -4,5 +4,5 @@ from src.website.views import Home, Explore
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
-    path('explore/', Explore.as_view(), name="home")
+    path('explore/', Explore.as_view(), name="explore")
 ]
